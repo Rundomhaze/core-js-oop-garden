353731f1ac77ea4b5465cacac286f3be41ed63a2
Narkas <64556642+nariolimonario@users.noreply.github.com>
2021-07-08 18:36:55 +0300

    Update flower.js

--- src/flower.js
+++ src/flower.js
@@ -1,11 +1,11 @@
-// Тебе пригодится класс Plant.
-// Не переживай, он уже доступен этому файлу
-// благодаря импорту с помощью require.
+
+// You will need the class Plant.
+// But do not worry, it's already available here because of the import require
 
 const Plant = require('../src/plant');
 
 class Flower {
-// Код тут.
+// Your code goes here.
 }
 
 module.exports = Flower;
