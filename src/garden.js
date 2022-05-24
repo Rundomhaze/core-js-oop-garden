d8532863d0f1f6ba7cfe68c8e01dbf77dc2084f9
Alex <petenkov2822@gmail.com>
2022-05-25 00:10:53 +0400

    Releases 0-1

--- src/garden.js
+++ src/garden.js
@@ -1,10 +1,29 @@
 class Garden {
-  constructor(gardenName, gardenLocation) {
-    this.name = gardenName;
-    this.location = gardenLocation;
+  constructor(name, location) {
+    this.name = name;
+    this.location = location;
     this.flowers = [];
-  }
+  };
 
+  addFlower(flower) {
+    return this.flowers.push(flower);
+  };
+
+  plant(flowers) {
+    flowers.forEach(element => this.flowers.push(element));
+  };
+
+  remove() {
+    return this.flowers.pop();
+  };
+
+  flowersByColor(color) {
+    return this.flowers.filter(el => el.flowerColor === color);
+  };
+
+  flowersByName(name) {
+    return this.flowers.filter(el => el.flowerName === name);
+  };
 
   /*
     Use object literal syntax to create a garden object with
